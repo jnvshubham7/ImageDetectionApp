9e330352d416f84ba8f18eb54b90acf197f9e55c
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2023-12-20 22:05:59 +0530

    Update MainActivity2.java

--- app/src/main/java/com/shubham/imagedetectionapp/MainActivity2.java
+++ app/src/main/java/com/shubham/imagedetectionapp/MainActivity2.java
@@ -2,8 +2,21 @@ package com.shubham.imagedetectionapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    Button selectButton;
+
+    Button uploadButton;
+
+    Button showUploadsButton;
+
+    ImageView image;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
